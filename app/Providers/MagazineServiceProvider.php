02a8d6c03cc74f316a9ac1747c3a58439d4a43fe
Nathan Lee <esoodev@gmail.com>
2018-05-08 14:31:54 +0900

    Fix: bound CommentService to the app through the provider.

--- app/Providers/MagazineServiceProvider.php
+++ app/Providers/MagazineServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Library\Services\MagazineService;
+use App\Library\Services\CommentService;
 use App\Library\Services\CategoryService;
 use App\Library\Services\TagService;
 
@@ -28,6 +29,10 @@ class MagazineServiceProvider extends ServiceProvider
     {
         $this->app->bind('App\Library\Services\MagazineService', function ($app) {
             return new MagazineService();
+        });
+        
+        $this->app->bind('App\Library\Services\CommentService', function ($app) {
+            return new CommentService();
         });
 
         $this->app->bind('App\Library\Services\CategoryService', function ($app) {
